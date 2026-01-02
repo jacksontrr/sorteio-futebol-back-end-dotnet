1548adae1e86b468a922a8b79e638d20cc0601e7
Jackson Torres <jackson.torres.1000@gmail.com>
2026-01-02 19:35:48 -0300

    Adicione suporte a CORS para o domínio https://www.softjack.com.br

--- Program.cs
+++ Program.cs
@@ -41,7 +41,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-            .WithOrigins("http://localhost:5173") // porta do Vite
+            .WithOrigins("http://localhost:5173").WithOrigins("https://www.softjack.com.br")
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
